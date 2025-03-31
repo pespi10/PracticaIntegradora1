f072156a0516b5da65fc077f211d7cb544a9f5e1
spinelliv@udesa.edu.ar <spinelliv@udesa.edu.ar>
2025-03-31 11:17:26 -0300

    express y routes

---
+++ practica1/routes/heroes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const heroeController = require('../controllers/heroeController');
+
+
+router.get('/', heroeController.list);
+router.get('/detalle/id/:id', heroeController.detail);
+router.get('/bio/id/:id/:ok?', heroeController.bio);
+
+module.exports = router;
